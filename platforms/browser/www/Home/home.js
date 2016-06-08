61bef58031570cb17516774fc5061fecd0d20582
mohamedfawzy96 <mohamedfawzydes@gmail.com>
2016-06-08 20:55:20 +0200

    sign in design updated

--- platforms/browser/www/Home/home.js
+++ platforms/browser/www/Home/home.js
@@ -1,51 +1,77 @@
-$(".tab1").click(function(){
+$(function(){
 
-  $("#body1").css({"display":"block"});
-  $("#body2").css({"display":"none"});
-  $(this).css({"border-bottom":"7px solid white"});
-  $(".tab2").css({"border":"0"});
-  $(".tab3").css({"border":"0"});
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        getImages();
+      } else {
+        // No user is signed in.
+      }
+    });
+    //var user = firebase.auth().currentUser;
+    //getImages();
+    $(".tab1").click(function(){
 
+    $("#body1").css({"display":"block"});
+    $("#body2").css({"display":"none"});
+    $(this).css({"border-bottom":"7px solid white"});
+    $(".tab2").css({"border":"0"});
+    $(".tab3").css({"border":"0"});
 
+  });
 
+  $(".tab2").click(function(){
 
+    $("#body2").css({"display":"block"});
+    $("#body1").css({"display":"none"});
+    $("tab").css({"border":"0"});
 
+    $(this).css({"border-bottom":"7px solid white"});
+    $(".tab1").css({"border":"0"});
+    $(".tab3").css({"border":"0"});
+
+  });
+
+  $(".tab3").click(function(){
+
+    $("#body2").css({"display":"block"});
+    $("#body1").css({"display":"none"});
+    $("tab").css({"border":"0"});
+
+    $(this).css({"border-bottom":"7px solid white"});
+    $(".tab1").css({"border":"0"});
+    $(".tab2").css({"border":"0"});
+
+  });
+
+  function handleFileSelect(evt) {
+    window.location = "../Send/send.html";
+  };
+
+  function getImages(){
+      var div = $('#body1');
+      newhtml = "";
+      var user = firebase.auth().currentUser;
+      var imagesRef = database.ref().child('memoriespic/'+user.uid);
+      imagesRef.once('value',function(snapshot){
+      snapshot.forEach(function (imageObj){
+        var st = imageObj.child("name").val();
+        var string = 'memories/'+user.uid + st;
+        var image = storageRef.child(string);
+        //imageObj.forEach(function (child){
+          //alert(child.val);
+        //});
+        //alert(imageObj.child("url").val());
+        var url = imageObj.child("url").val();
+        newhtml += "<img src="+url+" class=\"memoriespic\">";
+
+      });
+      div.html(newhtml);
+      //alert('done');
+    });
+  };
+
+  $("#sendimg").click(handleFileSelect);
+  //$('#files').hide();
+  //alert('loading images now!');
+  //$('#body1').remove();
 });
-
-$(".tab2").click(function(){
-
-  $("#body2").css({"display":"block"});
-  $("#body1").css({"display":"none"});
-  $("tab").css({"border":"0"});
-
-  $(this).css({"border-bottom":"7px solid white"});
-  $(".tab1").css({"border":"0"});
-  $(".tab3").css({"border":"0"});
-
-
-
-
-});
-
-$(".tab3").click(function(){
-
-  $("#body2").css({"display":"block"});
-  $("#body1").css({"display":"none"});
-  $("tab").css({"border":"0"});
-
-  $(this).css({"border-bottom":"7px solid white"});
-  $(".tab1").css({"border":"0"});
-  $(".tab2").css({"border":"0"});
-
-
-
-
-});
-
-
-
-$("#send").click(function(){
-  window.location = '../Send/Send.html'
-
-
-})
--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -24,9 +24,11 @@
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
 
-    document.addEventListener('deviceready', this.onDeviceReady, false);
-    $(".Signup").click(function(){
+$(function(){
 
+   document.addEventListener('deviceready', this.onDeviceReady, false);
+    $(".Signup").click(function(){
+      $('#sigin').id = "signup";
       $(this).css({"color":"#ec4b57"})
       $(this).css({"border-bottom":"8px solid #ec4b57"})
       $(".Signin").css({"color":"white"})
@@ -35,13 +37,10 @@
       $(".LeftDiv").css({"display":"none"})
       $(".RightFooter").css({"left":"0"})
       $(".LeftFooter").css({"display":"none"})
-
-
-
-    })
+    });
 
     $(".Signin").click(function(){
-
+      $('#signup').id = "sigin";
       $(this).css({"color":"#ec4b57"})
       $(this).css({"border-bottom":"8px solid #ec4b57"})
       $(".Signup").css({"color":"white"})
@@ -50,31 +49,122 @@
       $(".LeftDiv").css({"display":"block"})
       $(".RightFooter").css({"left":"100%"})
       $(".LeftFooter").css({"display":"block"})
-
-    })
-
-
-$("#signin").click(function(){
-
-  window.location = 'Home/home.html'
+    });
 
 
-})
+    $('#signup').click(function(){
+      var email = $('#emailup').val();
+      var password = $('#passwordup').val();
+      var currusername = $('#usernameup').val();
+      firebase.auth().createUserWithEmailAndPassword(email, password).then(function(){
+        alert('okaaaay!');
+        firebase.auth().signInWithEmailAndPassword(email, password).then(function(){
+          alert('signed in');
+          var user = firebase.auth().currentUser;
+          if (user) {
+            alert('user exists and signed in '+user.email);
+            usersRef.push().set({
+              email: user.email,
+              uid: user.uid,
+              username: currusername
+            });
+          } else {
+            alert('null user!!');
+          }
+        }).catch(function(error) {
+        alert('first '+ error);
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            alert('msg ' + errorMessage);
+            if (errorCode == 'auth/weak-password') {
+              alert('The password is too weak.');
+            } else {
+              console.error(error);
+            }
+          });
+      }).catch(function(error) {
+      alert('first '+ error);
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          alert('msg ' + errorMessage);
+          if (errorCode == 'auth/weak-password') {
+            alert('The password is too weak.');
+          } else {
+            console.error(error);
+          }
+        });
+      $('#signuppage').hide();
+      $('#signinpage').show();
+    });
 
+    $('#signin').click(function(){
+      alert('sign in y walaaa');
+      var email = $('#emailin').val();
+      var password = $('#passwordin').val();
+      firebase.auth().signInWithEmailAndPassword(email, password).then(function(){
+          alert('signed in');
+          var user = firebase.auth().currentUser;
+          if (user) {
+            alert('user exists and signed in '+user.email);
+            usersRef.on('value', function(snapshot){
+              snapshot.forEach(function (childSnapshot){
+                //alert(childSnapshot.child("email").val());
+                if(childSnapshot.child("uid").val()==user.uid){
+                  var currUser = childSnapshot;
+                  var currUserEmail = childSnapshot.child("email").val();
+                  var currUserUid = childSnapshot.child("uid").val();
+                    alert('found your user '+currUserEmail+"/"+currUserUid);
+                  }
+                  else{
+                    //alert('not this one '+childSnapshot.child("email").val());
+                  }
+                });
+              });
+            window.location = "Home/home.html"
+          } else {
+            alert('null user!!');
+          }
+        }).catch(function(error) {
+        alert('first '+ error);
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            alert('msg ' + errorMessage);
+            if (errorCode == 'auth/weak-password') {
+              alert('The password is too weak.');
+            } else {
+              console.error(error);
+            }
+          });
+    });
 
-    /*auth.onAuthStateChanged(function(user){
-      if(user){
-        alert("su");
-      }else {
-        alert("f");
-      }
-    });*/
+    function initApp() {
+      firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          alert('signed in');
+          var displayName = user.displayName;
+          var email = user.email;
+          var emailVerified = user.emailVerified;
+          var photoURL = user.photoURL;
+          var isAnonymous = user.isAnonymous;
+          var uid = user.uid;
+          var refreshToken = user.refreshToken;
+          var providerData = user.providerData;
 
+        } else {
+          alert('not signed in');
+        }
+      });
+    };
 
+    $('#facebookbtn').click(function(){
+        var auth = firebase.auth();
+        var provider = new firebase.auth.GoogleAuthProvider();
+        auth.signInWithPopup(provider).then(function(result){
+        var uid = result.user.uid;
+      }).catch(function (error){
+        alert("failed");
+        alert(error);
+      });
 
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-
-    // Update DOM on a Received Event
+    });
+});
