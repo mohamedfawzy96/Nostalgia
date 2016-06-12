5bb37aaa8c8fe06771abe97bd19e0f3bc1769da7
$pwd <mohabamr1@gmail.com>
2016-06-12 17:21:56 +0200

    memory database
    pushing memory in the posted list for the owner and also in the member
    list of the members

--- www/Home/home.js
+++ www/Home/home.js
@@ -7,8 +7,6 @@ $(function(){
       var url1 = listOfImages[counter]
       var date1= listOfimagesAttr[counter].child("date").val()
       var owner1= listOfimagesAttr[counter].child("owner").val()
-
-
       if((counter+1)<length1){
         var url2 = listOfImages[counter+1]
         var date2= listOfimagesAttr[counter+1].child("date").val()
@@ -23,21 +21,8 @@ $(function(){
       }
       counter++;
 
-
-
-
-
-
-
     }
-
-
-
-
-
-  }
-    var listOfimagesAttr = new Array();
-    var listOfImages = new Array();
+  };
 
 
     firebase.auth().onAuthStateChanged(function(user) {
@@ -48,78 +33,75 @@ $(function(){
         // No user is signed in.
       }
     });
-    //var user = firebase.auth().currentUser;
-    //getImages();
-    $(".tab1").click(function(){
 
+    $(".tab1").click(function(){
     $("#body1").css({"display":"block"});
     $("#body2").css({"display":"none"});
     $(this).css({"border-bottom":"7px solid white"});
     $(".tab2").css({"border":"0"});
     $(".tab3").css({"border":"0"});
-
   });
 
   $(".tab2").click(function(){
-
     $("#body2").css({"display":"block"});
     $("#body1").css({"display":"none"});
     $("tab").css({"border":"0"});
-
     $(this).css({"border-bottom":"7px solid white"});
     $(".tab1").css({"border":"0"});
     $(".tab3").css({"border":"0"});
-
   });
 
   $(".tab3").click(function(){
-
     $("#body2").css({"display":"block"});
     $("#body1").css({"display":"none"});
     $("tab").css({"border":"0"});
-
     $(this).css({"border-bottom":"7px solid white"});
     $(".tab1").css({"border":"0"});
     $(".tab2").css({"border":"0"});
-
   });
 
   function handleFileSelect(evt) {
     window.location = "../Send/Send.html";
   };
-
+  var memoriesArray = new Array();
+  var urlArray = new Array();
   function getImages(){
-
+      alert("get");
       var user = firebase.auth().currentUser;
-      var imagesRef = database.ref().child('memories/'+user.uid);
+      var users = database.ref().child("users");
+      var userInDatabase = users.child(user.uid);
+      var imagesRef = userInDatabase.child("posted");
       imagesRef.once('value',function(snapshot){
-      snapshot.forEach(function (imageObj){
-
-        var st = imageObj.child("name").val();
-        var string = 'memories/'+user.uid + st;
-        var image = storageRef.child(string);
-        var date = imageObj.child("date").val();
-
-        //imageObj.forEach(function (child){
-          //alert(child.val);
-        //});
-        //alert(imageObj.child("url").val());
-        var url = imageObj.child("url").val();
-        listOfimagesAttr.push(imageObj);
-        listOfImages.push(url);
-        alert(user.email)
-
-
-
+        memoriesArray = new Array();
+        urlArray = new Array();
+        memorySnap = snapshot.val();
+        //alert(snapshot);
+        //console.log(memorySnap);
+        alert(memorySnap);
+        memorySnap.forEach(function(memorySnapshot){
+          alert(memorySnapshot);
+          var res = memorySnapshot.split("/");
+          var memory = res.pop();
+          //alert(memory);
+              var url ="";
+              var imagesRef = database.ref().child('memories');
+              var imageRef = imagesRef.child(memory);
+              imageRef.child("url").once('value', function(data){
+                url = data.val();
+                //alert(url);
+                while (!url) {
+                  console.log('wait');
+                }
+                urlArray.push(url);
+                memoriesArray.push(snapshot);
+              });
+        });
+        alert(urlArray.length);
+        alert(memoriesArray.length);
+        fillMemoriesPanel(urlArray, memoriesArray);
       });
-      fillMemoriesPanel(listOfImages,listOfimagesAttr)
-      //div.html(newhtml);
-      //alert('done');
-    });
   };
 
   $("#sendimg").click(handleFileSelect);
-  //$('#files').hide();
-  //alert('loading images now!');
-  //$('#body1').remove();
+
 });
