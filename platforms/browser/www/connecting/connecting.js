e5dbf9d890627b61ae4118d05e026ac8eff3b941
mohamedfawzy96 <mohamedfawzydes@gmail.com>
2016-06-18 23:17:35 +0200

    connect

--- platforms/browser/www/connecting/connecting.js
+++ platforms/browser/www/connecting/connecting.js
@@ -1,3 +1,4 @@
+
 $("#find").click(function(){
   $(".searchView").css({"display":"block"});
   setTimeout(function(){
@@ -20,6 +21,19 @@ $("#friends").click(function(){
 
 
 })
+
+$(".request").click(function(){
+  $(".requestsView").css({"display":"block"});
+  setTimeout(function(){
+    $(".requestsView").css({"transform":"translateX(0)"});
+
+  },100)
+
+
+
+
+})
+
 $(".cancel").click(function(){
   $(".view").css({"transform":"translateX(800px)"});
 
@@ -34,7 +48,6 @@ $(".cancel").click(function(){
 
 })
 $(document).on('click tap', '.k', function(){
-  alert("hi")
   $(this).removeClass("AddFriend")
   $(this).html("Requested")
 
